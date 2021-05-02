2761ab0c11d1592508ef3a731c4bdb0d100823ff
Carlos Daniel <dev.cdfaria@gmail.com>
2021-05-02 01:54:35 -0300

    refactoring

--- SizeAdjust.js
+++ SizeAdjust.js
@@ -1,82 +1,58 @@
-Options = {
+const Options = {
   minWidth: 420,
   maxWidth: 1365,
   measure: "rem"
 },
 
-CSSSelectors = [
+const CSSSelectors = [
   {
     selector: "h1",
     propAndValue: [
-      {
-        property: "font-size",
-        min: 2.2,
-        max: 5,
-      },
-      {
-        property: "line-height",
-        min: 2.4,
-        max: 5.4,
-      }
+      { property: "font-size", min: 2.2, max: 5 },
+      { property: "line-height", min: 2.6, max: 5.4 }
     ]
   },
 
   {
     selector: "div",
     propAndValue: [
-      {
-        property: "width",
-        min: 25,
-        max: 50,
-      },
-      {
-        property: "height",
-        min: 10,
-        max: 20,
-      }
+      { property: "width", min: 25, max: 50 },
+      { property: "height", min: 10, max: 20 }
     ]
   }
 ]
 
 const SizeAdjust = {
+  createStyleEl() {
+    const styleEl = document.createElement('style')
+    document.getElementsByTagName('head')[0].appendChild(styleEl)
+    styleEl.insertAdjacentHTML("beforebegin", "<!-- Style injected by SizeAdjust (github.com/ruuuff) -->")
+  },
+
   scale(num, in_min, in_max, out_min, out_max) {
     let value = (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
     
     value <= out_min ? value = out_min : value
     value >= out_max ? value = out_max : value
-    
+
     return value
   },
 
-  scaler(min, max) {
-    const screenWidth = Number(window.innerWidth)
-
-    return SizeAdjust.scale(screenWidth, Number(Options.minWidth), Number(Options.maxWidth), Number(min), Number(max))
+  callScaleWithParameters(min, max) {
+    return SizeAdjust.scale(Number(document.documentElement.clientWidth), Number(Options.minWidth), Number(Options.maxWidth), Number(min), Number(max))
   },
 
-  createStyleEl() {
-    const head = document.getElementsByTagName('head')[0]
-    const style = document.createElement('style')
-    head.appendChild(style)
-    style.insertAdjacentHTML("beforebegin", "<!-- Style injected by SizeAdjust (github.com/ruuuff) -->")
-  },
-
-  sizeAdjust() {
-    const style = document.getElementsByTagName('style')[0]
-
+  innerStyles() {
+    const style = document.querySelector('head style:last-child')
     style.innerHTML = ""
 
-    CSSSelectors.forEach(obj => {
-      const { selector, propAndValue } = obj
-
+    CSSSelectors.forEach(({ selector, propAndValue }) => {
       style.insertAdjacentHTML("beforeend", `${selector} {`)
 
-      propAndValue.forEach(prop => {
-        const { property, min, max } = prop
+      propAndValue.forEach(({ property, min, max }) => {
+        const size = SizeAdjust.callScaleWithParameters(Number(min), Number(max)).toFixed(2)
 
-        const getSize = SizeAdjust.scaler(min, max).toFixed(1)
-
-        style.insertAdjacentHTML("beforeend", `  ${property}: ${String(getSize + Options.measure)};`)
+        style.insertAdjacentHTML("beforeend", `  ${property}: ${size + Options.measure};`)
       })
       style.insertAdjacentHTML("beforeend", `}
       `)
@@ -85,5 +61,5 @@ const SizeAdjust = {
 }
 
 SizeAdjust.createStyleEl()
-SizeAdjust.sizeAdjust()
-window.addEventListener('resize', SizeAdjust.sizeAdjust)
+SizeAdjust.innerStyles()
+window.addEventListener('resize', SizeAdjust.innerStyles)
